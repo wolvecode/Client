987fa0f7f7efa3d6e29cf35b918cc2b47fe840ad
wolvecode <wolvecode@gmail.com>
2020-02-03 23:42:44 +0100

    loader

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div className="row">
+      <div className="col-12 text-center">
+        <h3 className="font-weight-bold text-primary">Todoist</h3>
+      </div>
+    </div>
+  )
+}
